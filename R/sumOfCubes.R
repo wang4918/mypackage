04572c430016404212f016cafe701c9f5b79648d
wang4918 <35510669+wang4918@users.noreply.github.com>
2018-12-04 12:10:35 +0100

    should work???

---
+++ R/sumOfCubes.R
@@ -0,0 +1,10 @@
+#' @title Sum of cubes
+#' @description Returns the sum of the cube of twon given arguments
+#' @param x first value
+#' @param y second value
+#' @examples val <- sumofcubes(3, 4)
+#' @export
+
+sumofcubes <- function(x,y){
+  return(x^3+y^3)
+}
